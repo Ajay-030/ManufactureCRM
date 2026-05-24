7c330310589f62edbcfb8a17b547f9e8e0742f9d
ajay <ajayanguraj26@gmail.com>
2026-05-25 01:07:32 +0530

    Fix Vercel React routing

--- server/config/db.js
+++ server/config/db.js
@@ -8,7 +8,8 @@ const connectDB = async () => {
     console.log(`\x1b[32m[Database] MongoDB Connected: ${conn.connection.host}\x1b[0m`);
     global.useMockDb = false;
   } catch (error) {
-    console.error(`\x1b[31m[Database Error] Failed to connect to MongoDB at ${mongoUri}\x1b[0m`);
+   console.error(error);
+  console.error(`\x1b[31m[Database Error] Failed to connect to MongoDB at ${mongoUri}\x1b[0m`);
     console.warn('\x1b[33m[Database Warning] Please make sure MongoDB is installed and running locally, or configure MONGO_URI in a .env file.\x1b[0m');
     console.warn('\x1b[36m[Failsafe Mode] MongoDB not detected. Starting server in IN-MEMORY MOCK DATABASE MODE! All operations will load and save perfectly in-memory.\x1b[0m');
     global.useMockDb = true;
